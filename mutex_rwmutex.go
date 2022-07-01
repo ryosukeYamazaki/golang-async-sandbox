35121e46ba108fb354993341cf53faeda13b574b
ryosukeYamazaki <ryamazaki@c-fo.com>
2022-07-01 09:41:37 +0900

    :+1: rwmutexを使用する

--- mutex_rwmutex.go
+++ mutex_rwmutex.go
@@ -1,44 +1,58 @@
 package main
 
-import "fmt"
-import "sync"
+import (
+	"fmt"
+	"math"
+	"os"
+	"sync"
+	"text/tabwriter"
+	"time"
+)
 
-func main(){
-	var count int
-	var lock sync.Mutex
-
-	increment := func() {
-		lock.Lock()
-		defer lock.Unlock()
-		count++
-		fmt.Printf("Incrementing: %d\n", count)
+func rwmutex() {
+	producer := func(wg *sync.WaitGroup, l sync.Locker) {
+		defer wg.Done()
+		for i := 5; i > 0; i-- {
+			l.Lock()
+			l.Unlock()
+			time.Sleep(1)
+		}
 	}
 
-	decrement := func() {
-		lock.Lock()
-		defer lock.Unlock()
-		count--
-		fmt.Printf("Decrementing: %d\n", count)
+	observer := func(wg *sync.WaitGroup, l sync.Locker) {
+		defer wg.Done()
+		l.Lock()
+		defer l.Unlock()
 	}
 
-	var arithmetic sync.WaitGroup
-	for i := 0; i <= 5; i++{
-		arithmetic.Add(1)
-		go func(){
-			defer arithmetic.Done()
-			increment()
-		}()
+	test := func(count int, mutex, rwMutex sync.Locker) time.Duration {
+		var wg sync.WaitGroup
+		wg.Add(count + 1)
+		beginTestTime := time.Now()
+		go producer(&wg, mutex)
+		for i := count; i > 0; i-- {
+			go observer(&wg, rwMutex)
+		}
+
+		wg.Wait()
+
+		return time.Since(beginTestTime)
 	}
 
-	for i := 0; i <= 5; i++{
-		arithmetic.Add(1)
-		go func(){
-			defer arithmetic.Done()
-			decrement()
-		}()
+	tw := tabwriter.NewWriter(os.Stdout, 0, 1, 2, ' ', 0)
+	defer tw.Flush()
+
+	var m sync.RWMutex
+	fmt.Fprintf(tw, "Readers\tRWMutext\tMutex\n")
+	for i := 0; i < 20; i++ {
+		count := int(math.Pow(2, float64(i)))
+		fmt.Fprintf(
+			tw,
+			"%d\t%v\t%v\n",
+			count,
+			test(count, &m, m.RLocker()),
+			test(count, &m, &m),
+		)
 	}
 
-	arithmetic.Wait()
-
-	fmt.Printf("Arithmetic complete.")
 }
